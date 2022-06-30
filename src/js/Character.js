7b49ebd1cdb0bb81a4293e341f475e143e918386
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-06-30 20:15:48 +0300

    file added

--- src/js/Character.js
+++ src/js/Character.js
@@ -24,7 +24,7 @@ export default function Character(name,type) {
         throw new Error("Ошибка! количество символов в имени от 2 до 10!");
     }
 
-    if (!typeHeroe.includes(type)) {
+    if (!typeHero.includes(type)) {
         throw new Error("Ошибка! Такой персонаж не существует!");
         }
 }   
